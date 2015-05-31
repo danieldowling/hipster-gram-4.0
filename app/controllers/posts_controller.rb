cd156d8dc8d8a4426732cbf8c4874cd0c25dace9
Daniel Dowling <ddowling30@gmail.com>
2015-05-31 14:10:23 -0700

    created image uploader for posts, created post#new view, used resources for post routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,16 @@
 class PostsController < ApplicationController
+	def new
+    @user = current_user
+    @post = @user.posts.new
+  end
+
+  def create
+    @user = current_user
+    @post = @user.posts.new(post_params)
+      if @post.save
+        redirect_to posts_path
+      else
+        render :new
+      end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150531074429) do
+ActiveRecord::Schema.define(version: 20150531203244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "image_url"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -27,4 +37,5 @@ ActiveRecord::Schema.define(version: 20150531074429) do
     t.string   "image_url"
   end
 
+  add_foreign_key "posts", "users"
 end
