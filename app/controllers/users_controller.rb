b9f3a8be025fa04ec3859a033ee60d00182a571c
Daniel Dowling <ddowling30@gmail.com>
2015-06-05 11:35:24 -0700

    commiting before merge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
   end
 
   def new
