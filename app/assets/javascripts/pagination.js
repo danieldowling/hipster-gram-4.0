b0c2bf0341df62f34250621be011af1eea548c53
Daniel Dowling <ddowling30@gmail.com>
2015-06-25 15:19:49 -0700

    removed jquery gem

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -1,6 +1,6 @@
-$(document).on('page:update', function(){
-	$('.pagination a').on('click', function(event){
-		$.get(this.href, null, null, 'script');
-		event.preventDefault();
-	})
-});
+// $(document).on('page:update', function(){
+// 	$('.pagination a').on('click', function(event){
+// 		$.get(this.href, null, null, 'script');
+// 		event.preventDefault();
+// 	})
+// });
